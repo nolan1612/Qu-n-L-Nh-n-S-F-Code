6629e786ee94108c1210fa4347ca44fe8fa62bf3
Este4 <ledangminh1206@gmail.com>
2026-04-22 20:34:35 +0700

    sua file.h

--- includes/fileio.h
+++ includes/fileio.h
@@ -1,1 +1,13 @@
 //  Khai báo các hàm đọc/ghi file
+#ifndef FILEIO_H
+#define FILEIO_H
+
+#include "auth.h"
+
+void saveAccounts(Account list[], int count);
+int loadAccounts(Account list[]);
+
+void saveEvents(Event list[], int count);
+int loadEvents(Event list[]);
+
+#endif
